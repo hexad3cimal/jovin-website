0948fdec89a4a7469d447c65ad94013d816e9460
Jovin <jovin.war@gmail.com>
2020-10-03 23:03:17 +0530

    add responsiveness

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -3,33 +3,32 @@
     <TopNav></TopNav>
 
     <div class="title-wrapper">
-      <div class="title">
-        <img class="logo" src="../assets/images/default.png">
-      </div>
+      <div class="logo" />
     </div>
     <div class="tiles-container">
-      <Card v-for="skill in skills" :key="skill"  :title="skill" />
+      <Card v-for="skill in skills" :key="skill" :title="skill" />
     </div>
   </div>
 </template>
 
 <script>
-import {TopNav,Card} from "@/components/Home";
+import { TopNav, Card } from "@/components/Home";
 export default {
-  name: 'Home',
-  components: {Card, TopNav},
-  data : () => {
+  name: "Home",
+  components: { Card, TopNav },
+  data: () => {
     return {
-      skills : ['react', 'angular', 'vue', 'node', 'golang']
-    }
-  }
-}
+      skills: ["react", "angular", "vue", "node", "golang"],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @import "src/assets/css/colors.scss";
+@import "src/assets/css/breakpoints.scss";
 
-.home-wrapper{
+.home-wrapper {
   width: 100%;
   min-height: 100vh;
   background-image: radial-gradient($secondary 30%, $background 30%);
@@ -37,60 +36,59 @@ export default {
   flex-wrap: wrap;
   flex-direction: row;
 
-  .title-wrapper{
+  .title-wrapper {
     display: flex;
     justify-content: center;
     margin-top: 30vh;
-  }
+    width: 100%;
 
-  .tiles-container{
-    width:50%;
+    .logo {
+      width: 20%;
+      height: 50vh;
+      background-image: url("../assets/images/default.png");
+      background-repeat: no-repeat;
+      background-size: contain;
+
+      &:hover {
+        animation: shadow-wave 1.5s ease infinite;
+      }
+      @include respond-to("small") {
+        width: 60%;
+      }
+    }
+
+    @keyframes shadow-wave {
+      0% {
+        background-image: url("../assets/images/default.png");
+      }
+      20% {
+        background-image: url("../assets/images/redlogo.png");
+      }
+      40% {
+        background-image: url("../assets/images/orangelogo.png");
+      }
+      60% {
+        background-image: url("../assets/images/yellowlogo.png");
+      }
+      80% {
+        background-image: url("../assets/images/greenlogo.png");
+      }
+      90% {
+        background-image: url("../assets/images/greenlogo.png");
+      }
+      100% {
+        background-image: url("../assets/images/default.png");
+      }
+    }
+  }
+  .tiles-container {
+    width: 50%;
     margin-left: auto;
     margin-right: auto;
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
     align-content: space-evenly;
-  
-}
-}
-
-.logo:hover{
-  animation: shadow-wave 1.5s ease infinite;
-
-}
-@keyframes shadow-wave {
-  0% {
-    background-image: url('../assets/images/default.png');
-    width: 100%;
   }
-  20% {
-    background-image: url('../assets/images/redlogo.png');
-        width: 99%;
-  }
-  40% {
-     background-image: url('../assets/images/orangelogo.png');
-         width: 100%;
-  }
-  60% {
-        background-image: url('../assets/images/yellowlogo.png');
-         width: 99%;
-
-  }
-  80% {
-        background-image: url('../assets/images/greenlogo.png');
-         width: 100%;
-
-  }
-   90% {
-        background-image: url('../assets/images/greenlogo.png');
-         width: 99%;
-
-  }
-  100% {
-      background-image: url('../assets/images/default.png');
-         width: 100%;
-  }
-
 }
 </style>
