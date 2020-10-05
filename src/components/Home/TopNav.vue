b45cc26130a750ccd9c39ad81f2a0118f1861f8c
Jovin <jovin.war@gmail.com>
2020-10-05 22:07:16 +0530

    deployment build

--- src/components/Home/TopNav.vue
+++ src/components/Home/TopNav.vue
@@ -1,37 +1,89 @@
 <template>
-<header class="header">
-  <img class="logo" src="../../assets/images/logo-small.png" />
-  <div class="logo-text">jovin thariyath</div>
-</header>
+  <header class="header">
+    <div class="logo" />
+    <div class="logo-text">jovin thariyath</div>
+  </header>
 </template>
 
 <script>
 export default {
-name: "TopNav"
-}
+  name: "TopNav",
+};
 </script>
 
-<style scoped>
-.header{
+<style lang="scss" scoped>
+@import "src/assets/css/colors.scss";
+
+.header {
   height: 3rem;
   position: fixed;
-  background-color: #fff;
-  padding: .5rem;
+  background-color: $background;
+  padding: 0.5rem;
   width: 100%;
   z-index: 99;
   top: 0;
-  transition: all .4s;
+  transition: all 0.4s;
   display: flex;
-  flex-direction: row;;
+  flex-direction: row;
   align-items: flex-start;
 }
-.logo{
-  padding: 1rem;
-  width: 4rem;
+
+  .logo-text { 
+   margin-left: 1rem;
+   color: white; 
+   font: bold 2rem Helvetica, Arial, Sans-Serif;
+   text-shadow: 1px 1px #fe4902, 
+                2px 2px #fe4902, 
+                3px 3px #fe4902;
+   -webkit-transition: all 0.12s ease-out;
+   -moz-transition:    all 0.12s ease-out;
+   -ms-transition:     all 0.12s ease-out;
+   -o-transition:      all 0.12s ease-out;
+
+   &:hover {
+   position: relative; 
+   top: -3px; 
+   left: -3px; 
+   text-shadow: 1px 1px #fe4902, 
+                2px 2px #fe4902, 
+                3px 3px #fe4902, 
+                4px 4px #fe4902, 
+                5px 5px #fe4902, 
+                6px 6px #fe4902;
 }
-.logo-text{
-  color: black ;
-  font-size: 1.2rem;
-  padding: 1.5rem .5rem;
+}
+
+.logo {
+  background-image: url("../../assets/images/default.png");
+  background-repeat: no-repeat;
+  background-size: contain;
+  padding: 1.7rem;
+  &:hover {
+    animation: shadow-wave 1.5s ease infinite;
+  }
+
+  @keyframes shadow-wave {
+    0% {
+      background-image: url("../../assets/images/default.png");
+    }
+    20% {
+      background-image: url("../../assets/images/redlogo.png");
+    }
+    40% {
+      background-image: url("../../assets/images/orangelogo.png");
+    }
+    60% {
+      background-image: url("../../assets/images/yellowlogo.png");
+    }
+    80% {
+      background-image: url("../../assets/images/greenlogo.png");
+    }
+    90% {
+      background-image: url("../../assets/images/greenlogo.png");
+    }
+    100% {
+      background-image: url("../../assets/images/default.png");
+    }
+  }
 }
 </style>
