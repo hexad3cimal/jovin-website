95208a32bcaa716bb93981c91bd3fd09b5c04176
hexad3cimal <jovin.war@gmail.com>
2021-03-23 22:22:07 +0530

    add dist

---
+++ dist/precache-manifest.11d004582f04b30e21d0d934932b8431.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da338fcdecc6d4c2e7d8",
+    "url": "/css/app.04154b65.css"
+  },
+  {
+    "revision": "053b5b8db3c0ed9d215fb36b6cd55238",
+    "url": "/img/jovin.053b5b8d.png"
+  },
+  {
+    "revision": "7153fac2e70690edacc1074493f8f13f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f0ef8e569b288dfadf1",
+    "url": "/js/about.0973fbaf.js"
+  },
+  {
+    "revision": "da338fcdecc6d4c2e7d8",
+    "url": "/js/app.f5ecbd3a.js"
+  },
+  {
+    "revision": "9bb8b242add17bd4c877",
+    "url": "/js/chunk-vendors.28075f57.js"
+  },
+  {
+    "revision": "f78cd8de333143188f631323c36ecd99",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
